834360dfecbcb41b08e552d3704593360fd31e31
Nolan Lawson <nlawson@salesforce.com>
2023-06-26 08:02:21 -0700

    fix: double license header

--- packages/lwc/module-resolver.js
+++ packages/lwc/module-resolver.js
@@ -3,10 +3,5 @@
  * All rights reserved.
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
- */ /*
- * Copyright (c) 2023, salesforce.com, inc.
- * All rights reserved.
- * SPDX-License-Identifier: MIT
- * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 export * from '@lwc/module-resolver';
